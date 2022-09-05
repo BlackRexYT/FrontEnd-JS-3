a325015046a5fd2ec864e83445a45cee42419790
Reks <fahadadsani@gmail.com>
2022-09-05 09:44:56 +0300

    commit 9

--- script.js
+++ script.js
@@ -87,12 +87,18 @@
 
 /* 8 */
 
-function reverseString(str){
-    let reversedString = ''
-    for (i=0;i<str.length;i++){
-        reversedString = str[i] + reversedString
-    }
-    return reversedString
-}
 
-console.log(reverseString('test'))
+// function reverseString(str){
+//     let reversedString = ''
+//     for (i=0;i<str.length;i++){
+//         reversedString = str[i] + reversedString
+//     }
+//     return reversedString
+// }
+
+// console.log(reverseString('fahad'))
+
+
+/* 9 */
+
+
