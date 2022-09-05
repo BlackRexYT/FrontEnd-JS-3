39d907aa0e3f873a9adfe5bfa2990591e7ea1206
Reks <fahadadsani@gmail.com>
2022-09-05 09:36:46 +0300

    commit 6

--- script.js
+++ script.js
@@ -19,11 +19,67 @@
 //     return arr[arr.length-1]
 // }
 
-function arrSum(arr){
-    total = 0
+// function arrSum(arr){
+//     let total = 0
+//     for (i=0;i<arr.length;i++){
+//         total += arr[i]
+//     }
+//     console.log(total)
+// }
+// arrSum([1,2,3,4,6,7])
+
+
+/* 5 */
+
+// function progressiveSum(num){
+//     let sum = 0
+//     for (i=num;i!=0;i--){
+//         sum += i
+//     }
+//     return sum
+// }
+
+// console.log(progressiveSum(3))
+
+
+/* 6 */
+
+
+// function returnTime(num){
+//     let timerMin = Math.floor(num / 60);
+//     let timerSec = num % 60;
+
+//     if (timerMin < 10){
+//         if (timerSec < 10){
+//             return '0' + timerMin + ':0' + timerSec
+//         }
+//         else{
+//             return '0' + timerMin + ':' + timerSec
+//         }
+//     }
+//     else{
+//         if (timerSec < 10){
+//             return timerMin + ':0' + timerSec
+//         }
+//         else{
+//             return timerMin + ':' + timerSec
+//         }
+//     }
+// }
+
+// console.log(returnTime(125))
+
+
+/* 7 */
+
+
+function getMax(arr){
+    let temp = arr[0]
     for (i=0;i<arr.length;i++){
-        total += arr[i]
+        if (temp < arr[i]){
+            temp = arr[i]
+        }
     }
-    console.log(total)
+    return temp
 }
-arrSum([1,2,3,4,6,7])
+console.log(getMax([1,200,3,4,-100,300]))
